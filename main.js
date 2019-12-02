57b2ef03458ca2ac04698554aaa9401f1e4341b5
Drew Mayo <53830148+drew687@users.noreply.github.com>
2019-12-02 12:32:11 -0600

    French club

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 function french() {
   var french = "Le club français se rendra dans différentes écoles d’Elémentry et présentera des spectacles de marionnettes en français.";
   var french2 = "Le babillard est l'endroit où nous allons mettre l'information mensuelle en français. Les choses que nous mettons sur le tableau bulltin sont le mot français, la phrase française et le pays français.";
+  var french3 = "Les étudiants du club français sont des personnes qui veulent en savoir plus sur la culture française que ce qu’ils apprennent déjà en classe. Ils profitent de leur temps avec d'autres personnes partageant les mêmes intérêts. Les membres aiment participer à la culture française avec des amis qui veulent aussi s'amuser. Tout le monde dans le club aime se réunir et participer à des activités amusantes. Le club a beaucoup de gens qui acceptent et qui sont ouverts à de nouvelles amitiés.";
   var active = document.getElementById("activities1").innerHTML;
   var active2 = document.getElementById("activities2").innerHTML;
   if (active !== french) {
